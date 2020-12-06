b42033a0e559c40d3aafc0ff4a472f95316f5f73
September <vitaliiseptember@gmail.com>
2020-12-06 19:38:58 +0200

    tableMultiply: draw Tables in line

--- nix.local/php/multiplytable.php
+++ nix.local/php/multiplytable.php
@@ -1,13 +1,29 @@
-<?php
-multiply(1,2);
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/multiplyTable.css">
+    <title>Multi table</title>
+</head>
+<body>
+<div class="m__table">
+    <table border="1">
+     <?php
+            $rows = 10;
+            $cols = 5;
+                for($tr = 1; $tr <= $rows;$tr++) {
+                    echo "\t\t<tr>\n";
+                    for($td = 1; $td <= $cols;$td++){
+                        echo "\t\t<td>{$td} x {$tr} = ".$td * $tr ."</td>\n";
+                    }
+                    echo "</tr>\n";
+                }
+    ?>
+</div>
 
-
-
-
-
-
-function multiply($multipl){
-    for($i = 0; $i < 10; $i++){
-        echo "$multipl x $i = ". $multipl * $i ."<br>";
-    }
-}
+</table>
+</body>
+</html>
