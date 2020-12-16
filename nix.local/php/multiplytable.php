5cb5295129e5d501c966e32ff66b2ebb3a11dd77
September <vitaliiseptember@gmail.com>
2020-12-16 15:24:49 +0200

    fix css for back__btn multiTable

--- nix.local/php/multiplytable.php
+++ nix.local/php/multiplytable.php
@@ -5,22 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/multiplyTable.css">
+    <link rel="stylesheet" href="../css/table.css">
     <title>Multi table</title>
 </head>
-<body class="body">
+<body>
 
 <div class="back__btn">
-    <a   style="display: inline-block;
-         padding: 10px 15px;
-         border: 2px solid rgb(176,252,0);
-         border-radius: 25px;
-         margin-top: 10px;
-         text-decoration: none;
-         color: rgb(176,252,0);
-         font-size: 16px;
-         font-weight: bold;"
-       href="../index.html">BACK</a>
+    <a href="../index.html">BACK</a>
 </div>
 
 <div class="m__table">
